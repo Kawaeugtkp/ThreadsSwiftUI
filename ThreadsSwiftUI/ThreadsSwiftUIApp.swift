d11fedb7082816bbdba722e753b1016db8d58772
川尻辰義 <grabdepa@gmail.com>
2023-09-03 16:17:41 +0900

    Initial Commit

---
+++ ThreadsSwiftUI/ThreadsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsSwiftUIApp.swift
+//  ThreadsSwiftUI
+//
+//  Created by 川尻辰義 on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
